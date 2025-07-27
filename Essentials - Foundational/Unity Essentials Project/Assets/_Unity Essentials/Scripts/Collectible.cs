aec4015716bee23eabd02d8885794f58a5554d56
jamisousa <jamiledsousa@gmail.com>
2025-07-27 11:35:21 -0300

    feat: adding collectible and door opener scripts

--- Essentials - Foundational/Unity Essentials Project/Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Essentials - Foundational/Unity Essentials Project/Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -8,6 +8,7 @@ public class Collectible : MonoBehaviour
 
     public float rotationSpeed;
 
+    public GameObject onCollectEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,24 @@ public class Collectible : MonoBehaviour
         transform.Rotate(0, rotationSpeed, 0);
     }
 
-  
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Add logic for when the player collects the item, e.g., increase score.
+
+
+            //Instantiate the effect at the collectible's position and rotation.
+            Instantiate(onCollectEffect, transform.position, transform.rotation);
+
+            Debug.Log("Collectible collected!");
+            Destroy(gameObject); // Destroy the collectible after collection.
+
+       
+        }
+    }
+
+
 
 
 }
